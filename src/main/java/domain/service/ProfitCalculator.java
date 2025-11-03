63bdf2bdf70575dd80b56ba0f5e488d3e92aae92
HeeJeongSeo <heejung9965@naver.com>
2025-11-03 19:15:25 +0900

    수익률 계산 및 출력 기능 구현

--- src/main/java/domain/service/ProfitCalculator.java
+++ src/main/java/domain/service/ProfitCalculator.java
@@ -1,5 +1,16 @@
 package domain.service;
 
-public class ProfitCalculator {
+import domain.value.Rank;
+import java.util.Map;
 
+public class ProfitCalculator {
+    public double calculateProfitRate(Map<Rank, Integer> results, int purchaseAmount) {
+        
+        //등수별 당첨 개수 * 상금
+        double totalPrize = results.entrySet().stream()
+                .mapToDouble(entry -> entry.getKey().getPrize() * entry.getValue())
+                .sum();
+        
+        return (totalPrize / purchaseAmount) * 100;
+    }
 }
--- src/main/java/views/OutputView.java
+++ src/main/java/views/OutputView.java
@@ -6,6 +6,7 @@ import domain.value.Rank;
 import java.util.Map;
 
 public class OutputView {
+    
     public void printPurchasedLottos(LottoTicket lottoTicket) {
         System.out.println(lottoTicket.size() + "개를 구매했습니다.");
         lottoTicket.getTickets().forEach(lotto ->
@@ -15,6 +16,11 @@ public class OutputView {
     public void printStatistics(Map<Rank, Integer> results, double profitRate) {
         System.out.println("당첨 통계");
         System.out.println("---");
+        for (Rank rank : Rank.values()) {
+            //꽝제외 
+            if (rank == Rank.NONE) continue;
+            System.out.printf("%s - %d개%n", rank.getDescription(), results.getOrDefault(rank, 0));
+        }
         System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }
