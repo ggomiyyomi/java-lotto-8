3aac75ca7c80b7a2fe0439f30426ffda08a28777
HeeJeongSeo <heejung9965@naver.com>
2025-11-03 19:19:58 +0900

    feat : 전체 흐름 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,5 +1,44 @@
 package controller;
 
-public class LottoController {
+import domain.model.LottoTicket;
+import domain.model.WinningLotto;
+import domain.service.LottoMachine;
+import domain.service.LottoResultCalculator;
+import domain.service.ProfitCalculator;
+import domain.value.Rank;
+import utils.Validator;
+import views.InputView;
+import views.OutputView;
 
+import java.util.List;
+import java.util.Map;
+
+public class LottoController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final LottoMachine lottoMachine = new LottoMachine();
+    private final LottoResultCalculator resultCalculator = new LottoResultCalculator();
+    private final ProfitCalculator profitCalculator = new ProfitCalculator();
+
+    public void run() {
+        try {
+            int purchaseAmount = inputView.readPurchaseAmount();
+            Validator.validatePurchaseAmount(purchaseAmount);
+
+            LottoTicket ticket = lottoMachine.purchase(purchaseAmount);
+            outputView.printPurchasedLottos(ticket);
+
+            List<Integer> winningNumbers = inputView.readWinningNumbers();
+            int bonusNumber = inputView.readBonusNumber();
+            WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+            Map<Rank, Integer> results = resultCalculator.calculate(ticket, winningLotto);
+            double profitRate = profitCalculator.calculateProfitRate(results, purchaseAmount);
+
+            outputView.printStatistics(results, profitRate);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            run(); // 재시작
+        }
+    }
 }
