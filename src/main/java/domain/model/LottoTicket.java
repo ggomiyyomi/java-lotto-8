6dca1422881e575f3f606aa2934cd5971a318722
HeeJeongSeo <heejung9965@naver.com>
2025-11-03 18:39:59 +0900

    feat: 로또 번호 자동 생성 기능 구현

--- src/main/java/domain/model/LottoTicket.java
+++ src/main/java/domain/model/LottoTicket.java
@@ -1,5 +1,19 @@
 package domain.model;
 
-public class LottoTicket {
+import java.util.List;
 
+public class LottoTicket {
+    private final List<Lotto> tickets;
+
+    public LottoTicket(List<Lotto> tickets) {
+        this.tickets = tickets;
+    }
+
+    public List<Lotto> getTickets() {
+        return tickets;
+    }
+
+    public int size() {
+        return tickets.size();
+    }
 }
--- src/main/java/domain/service/LottoMachine.java
+++ src/main/java/domain/service/LottoMachine.java
@@ -1,5 +1,23 @@
 package domain.service;
 
-public class LottoMachine {
+import domain.model.Lotto;
+import domain.model.LottoTicket;
+import utils.LottoNumberGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+
+    public LottoTicket purchase(int amount) {
+        int count = amount / LOTTO_PRICE;
+        List<Lotto> tickets = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            tickets.add(new Lotto(LottoNumberGenerator.generateNumbers()));
+        }
+
+        return new LottoTicket(tickets);
+    }
 }
