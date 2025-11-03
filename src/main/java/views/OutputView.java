2dd6cde235a22c58c225a1384e68dce86df33978
HeeJeongSeo <heejung9965@naver.com>
2025-11-03 18:50:57 +0900

    feat: 로또 번호 출력 기능 구현

--- src/main/java/views/OutputView.java
+++ src/main/java/views/OutputView.java
@@ -1,5 +1,20 @@
 package views;
 
-public class OutputView {
+import domain.model.LottoTicket;
+import domain.value.Rank;
 
+import java.util.Map;
+
+public class OutputView {
+    public void printPurchasedLottos(LottoTicket lottoTicket) {
+        System.out.println(lottoTicket.size() + "개를 구매했습니다.");
+        lottoTicket.getTickets().forEach(lotto ->
+                System.out.println(lotto.getNumbers()));
+    }
+
+    public void printStatistics(Map<Rank, Integer> results, double profitRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
+    }
 }
